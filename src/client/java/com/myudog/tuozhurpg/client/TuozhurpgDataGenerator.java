356fcca1df72b46e6c52386e30dc7db8c0cff9d1
Chiayu <chiayu.myu@icloud.com>
2026-01-14 11:46:19 +0800

    消滅寄生蟲，專案正式啟動

---
+++ src/client/java/com/myudog/tuozhurpg/client/TuozhurpgDataGenerator.java
@@ -0,0 +1,12 @@
+package com.myudog.tuozhurpg.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TuoZhuRpgDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
